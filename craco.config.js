1b75fd92c8b7d9ac67816865357897e10499a457
moon <1842298680@qq.com>
2022-04-09 16:37:09 +0800

    chore(project): add craco config file

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const resolve = dir => path.resolve(__dirname, dir);
+const CracoLessPlugin = require("craco-less");
+module.exports = {
+    webpack: {
+        alias: {
+            "@": resolve("src"),
+        },
+    },
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: { "@primary-color": "#1DA57A" },
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
+};
