840caab01e0adc100a8a81439db7ad7627e21891
moon <1842298680@qq.com>
2022-04-09 17:16:13 +0800

    feat(project): add data file

---
+++ src/configs/local-data.ts
@@ -0,0 +1,113 @@
+export const headerLinks = [
+    {
+        title: "发现音乐",
+        link: "/discover",
+    },
+    {
+        title: "我的音乐",
+        link: "/mine",
+    },
+    {
+        title: "朋友",
+        link: "/friend",
+    },
+    {
+        title: "商城",
+        link: "https://music.163.com/store/product",
+    },
+    {
+        title: "音乐人",
+        link: "https://music.163.com/nmusician/web/index#/",
+    },
+    {
+        title: "下载客户端",
+        link: "https://music.163.com/#/download",
+    },
+];
+
+export const footerLinks = [
+    {
+        title: "服务条款",
+        link: "https://st.music.163.com/official-terms/service",
+    },
+    {
+        title: "隐私政策",
+        link: "https://st.music.163.com/official-terms/privacy",
+    },
+    {
+        title: "儿童隐私政策",
+        link: "https://st.music.163.com/official-terms/children",
+    },
+    {
+        title: "版权投诉指引",
+        link: "https://music.163.com/st/staticdeal/complaints.html",
+    },
+    {
+        title: "意见反馈",
+        link: "#",
+    },
+    {
+        title: "联系我们",
+        link: "https://mp.music.163.com/600948c936c13f4d09752e73/contact-us-web/index.html?source=Music-Main-Station",
+    },
+    {
+        title: "广告合作",
+        link: "https://music.163.com/ui/resource",
+    },
+    {
+        title: "廉正举报",
+        link: "https://jubao.163.com/",
+    },
+];
+
+export const footerImages = [
+    {
+        title: "Amped Studio",
+        link: "https://web-amped.music.163.com/",
+    },
+    {
+        title: "用户认证",
+        link: "https://music.163.com/st/userbasic#/auth",
+    },
+
+    {
+        title: "独立音乐人",
+        link: "https://music.163.com/musician/artist",
+    },
+    {
+        title: "赞赏",
+        link: "https://music.163.com/web/reward",
+    },
+    {
+        title: "视频奖励",
+        link: "https://music.163.com/st/ncreator/revenue-plan",
+    },
+];
+
+// discover中的数据
+export const disCoverMenu = [
+    {
+        title: "推荐",
+        link: "recommend",
+    },
+    {
+        title: "排行榜",
+        link: "chart",
+    },
+    {
+        title: "歌单",
+        link: "playlist",
+    },
+    {
+        title: "主播电台",
+        link: "radio",
+    },
+    {
+        title: "歌手",
+        link: "singer",
+    },
+    {
+        title: "新碟上架",
+        link: "newsongs",
+    },
+];
