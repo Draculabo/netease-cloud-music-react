3d028f72484aefdaf64747d1326c2ad3299493ff
moon <1842298680@qq.com>
2022-04-09 17:14:01 +0800

    feat(component): add CMHeader

---
+++ src/components/CMHeader/CMHeader.tsx
@@ -0,0 +1,44 @@
+import { memo } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { HeaderLeft, HeaderRight, HeaderWrapper } from "./style";
+import { headerLinks } from "@/configs/local-data";
+export const CMHeader = memo(() => {
+    return (
+        <HeaderWrapper>
+            <div className="content wrap-v1">
+                <HeaderLeft>
+                    <h1 className="logo">
+                        <Link to={"/"}></Link>
+                    </h1>
+                    {headerLinks.map((value, index) => {
+                        return index < 3 ? (
+                            <NavLink key={index} to={value.link}>
+                                {value.title}
+                                <sub></sub>
+                            </NavLink>
+                        ) : (
+                            <a key={index} href={value.link} rel={"noreferrer"} target={"_blank"}>
+                                {value.title}
+                            </a>
+                        );
+                    })}
+                </HeaderLeft>
+                <HeaderRight>
+                    <div className="search-box">
+                        <input name="search" placeholder="音乐/视频/电台/用户" type="search" />
+                    </div>
+                    <a
+                        className="author-centre"
+                        href="https://music.163.com/login?targetUrl=%2Fcreatorcenter"
+                        rel={"noreferrer"}
+                        target={"_blank"}
+                    >
+                        创作者中心
+                    </a>
+                    <a className="login">登录</a>
+                </HeaderRight>
+            </div>
+            <div className="divider"></div>
+        </HeaderWrapper>
+    );
+});
---
+++ src/components/CMHeader/style.ts
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+import spriteLogo from "@/assets/img/sprite_01.png";
+export const HeaderWrapper = styled.div`
+    background-color: #333;
+    .content {
+        display: flex;
+        justify-content: space-between;
+        font-size: 14px;
+        height: 70px;
+        .logo {
+            width: 159px;
+            height: 70px;
+            margin-right: 20px;
+            background: url(${spriteLogo}) no-repeat;
+            > a {
+                display: block;
+                width: inherit;
+                height: inherit;
+            }
+        }
+    }
+    .divider {
+        height: 5px;
+        background-color: #c20c0c;
+    }
+`;
+export const HeaderLeft = styled.div`
+    display: flex;
+    > a {
+        position: relative;
+
+        height: inherit;
+        line-height: 50px;
+        padding: 10px 20px;
+        color: #eee;
+    }
+    sub {
+        content: "";
+        display: none;
+        border: 7px solid transparent;
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        border-bottom-color: #f00;
+        transform: translate(-50%, 0);
+    }
+    > a {
+        &:hover {
+            color: #fff;
+            background-color: #000;
+        }
+    }
+    .active {
+        color: #fff;
+        background-color: #000;
+        sub {
+            display: block;
+        }
+    }
+`;
+export const HeaderRight = styled.div`
+    display: flex;
+    align-items: center;
+    .search-box {
+        position: relative;
+        width: 158px;
+        height: 32px;
+        input {
+            width: inherit;
+            height: inherit;
+            padding-left: 30px;
+            border-radius: 20px;
+        }
+        &::before {
+            content: "";
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            width: 14px;
+            height: 14px;
+            background: url(${spriteLogo}) no-repeat -10px -108px;
+            background-color: #fff;
+        }
+    }
+    .author-centre {
+        font-size: 12px;
+        color: #d3d3d3;
+        padding: 5px 15px;
+        border: 1px solid #7a7a7a;
+        border-radius: 25px;
+        margin-left: 20px;
+        vertical-align: bottom;
+        &:hover {
+            color: #fff;
+            border-color: #fff;
+        }
+    }
+    .login {
+        padding: 10px 15px;
+        font-size: 13px;
+        color: #ccc;
+        &:hover {
+            color: #fff;
+        }
+    }
+`;
