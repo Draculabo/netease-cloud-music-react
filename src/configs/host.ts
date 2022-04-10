09a5eebde4442facaa94bb1553aa70e27406bb80
moon <1842298680@qq.com>
2022-04-11 00:01:02 +0800

    feat(project): config axios

---
+++ src/configs/host.ts
@@ -0,0 +1,14 @@
+export default {
+    mock: {
+        API: "",
+    },
+    test: {
+        API: "https://netease-cloud-music-api-eight-ochre-11.vercel.app",
+    },
+    development: {
+        API: "https://netease-cloud-music-api-eight-ochre-11.vercel.app",
+    },
+    production: {
+        API: "https://netease-cloud-music-api-eight-ochre-11.vercel.app",
+    },
+};
---
+++ src/utils/request.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosRequestHeaders } from "axios";
+import proxy from "@/configs/host";
+const request = axios.create({
+    baseURL: proxy.development.API,
+    timeout: 4000,
+    withCredentials: true,
+});
+request.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("ncm-tk-dra");
+        config.params = {
+            ...config.data,
+
+            realIP: "192.168.130.132",
+            id: "11121",
+        };
+
+        if (token !== null) {
+            (config.headers as AxiosRequestHeaders)["Authorization"] = token;
+        }
+        return config;
+    },
+    err => {
+        console.log(err);
+
+        return Promise.reject(err);
+    },
+);
+request.interceptors.response.use(
+    res => {
+        if (res.status === 200) {
+            return res.data;
+        }
+        return Promise.reject(res?.data);
+    },
+    err => {
+        console.log(err);
+        Promise.reject(err);
+    },
+);
+export default request;
