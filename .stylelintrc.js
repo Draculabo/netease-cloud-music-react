f248f6edd9a23a1f8563fedac2bc2776d69455bb
moon <1842298680@qq.com>
2022-04-13 15:05:25 +0800

    chore(project): add a  rule

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: "stylelint-config-standard",
     rules: {
         "selector-list-comma-space-after": "always-single-line",
+        "disable-next-line": "warn",
         indentation: "never",
     },
 };
