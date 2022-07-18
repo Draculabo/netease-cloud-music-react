3f86e4e7ea1d13efa463b799adbe38309857e80d
degulabo <1842298680@qq.com>
2022-07-18 20:28:25 +0800

    feat(component): add comment list skeleton

--- src/components/CommentList/CommentList.tsx
+++ src/components/CommentList/CommentList.tsx
@@ -4,6 +4,7 @@ import { take } from "lodash";
 import { memo } from "react";
 import { Link } from "react-router-dom";
 import { CommentListWrapper } from "./CommentList.style";
+import CommentListSkeleton from "./CommentListSkeleton";
 export interface CreatorType {
     userId: number;
     nickname: string;
@@ -29,45 +30,52 @@ const CommentList = memo((props: CommentListPropsType) => {
     const comments: CommentType[] = props.isHot
         ? take(props?.commentList, 15)
         : props?.commentList || [];
+
     return (
         <CommentListWrapper>
             <div className="comment-list-title">
                 <h3 className="font-bold">{props.title}</h3>
             </div>
             <div className="comment-list">
-                {comments.map(comment => {
-                    return (
-                        <div className="comment-item" key={comment.commentId}>
-                            <div
-                                className="avatarUrl"
-                                style={{ backgroundImage: `url(${comment.user.avatarUrl})` }}
-                            >
-                                <Link
-                                    className="block-a"
-                                    to={linkToUserHomePage(comment.user.userId)}
-                                ></Link>
-                            </div>
-                            <div className="content-field">
-                                <div className="name-content">
-                                    <Link to={linkToUserHomePage(comment.user.userId)}>
-                                        {comment.user.nickname}
-                                    </Link>
-                                    ： {comment.content}
+                {comments.length === 0 ? (
+                    <CommentListSkeleton />
+                ) : (
+                    comments?.map(comment => {
+                        return (
+                            <div className="comment-item" key={comment.commentId}>
+                                <div
+                                    className="avatarUrl"
+                                    style={{ backgroundImage: `url(${comment.user.avatarUrl})` }}
+                                >
+                                    <Link
+                                        className="block-a"
+                                        to={linkToUserHomePage(comment.user.userId)}
+                                    />
                                 </div>
+                                <div className="content-field">
+                                    <div className="name-content">
+                                        <Link to={linkToUserHomePage(comment.user.userId)}>
+                                            {comment.user.nickname}
+                                        </Link>
+                                        ： {comment.content}
+                                    </div>
 
-                                <div className="meta-data">
-                                    <div className="date">{formatToYMS(comment.time)}</div>
-                                    <div className="thumb-replay">
-                                        <div className="thumb-icon"></div>
-                                        <div className="thumb-count">({comment.likedCount})</div>
-                                        <div className="split">|</div>
-                                        <div className="replay">回复</div>
+                                    <div className="meta-data">
+                                        <div className="date">{formatToYMS(comment.time)}</div>
+                                        <div className="thumb-replay">
+                                            <div className="thumb-icon" />
+                                            <div className="thumb-count">
+                                                ({comment.likedCount})
+                                            </div>
+                                            <div className="split">|</div>
+                                            <div className="replay">回复</div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })
+                )}
             </div>
         </CommentListWrapper>
     );
