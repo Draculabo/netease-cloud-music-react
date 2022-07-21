09b9755d5b258e07a6469426e26e454768d02425
degulabo <1842298680@qq.com>
2022-07-21 20:34:46 +0800

    fix(component): fix  commentlist data without refreshing

--- src/pages/Discover/Ranking/Ranking.tsx
+++ src/pages/Discover/Ranking/Ranking.tsx
@@ -2,33 +2,30 @@ import CommentList from "@/components/CommentList/CommentList";
 import { CommentType, getComment, getPlayListDetail } from "@/services";
 import { useRequest } from "ahooks";
 import { Skeleton } from "antd";
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect } from "react";
 import { useParams } from "react-router";
 import { RankingInformation, RankingTable } from "./components";
 import { RankingLeft } from "./components/RankingLeft";
 import { RankingRightWrapper, RankingWrapper } from "./style";
 
 const Ranking = memo(() => {
-    const [playList, setPlayList] = useState<any>(null);
     const { id = "19723756" } = useParams();
-    const { loading: rankingLoading, run: rankingDetailRun } = useRequest<any, any>(
-        () => getPlayListDetail(id),
-        {
-            onSuccess(res) {
-                setPlayList(res?.playlist);
-            },
-            retryCount: 3,
-            retryInterval: 3000,
-            cacheKey: "playlist-detail",
-        },
-    );
-    const { loading: commentLoading, data: commentData } = useRequest(() =>
+    const {
+        data: res,
+        loading: rankingLoading,
+        run: rankingDetailRun,
+    } = useRequest<any, any>(() => getPlayListDetail(id), {
+        retryCount: 3,
+        retryInterval: 3000,
+        cacheKey: "playlist-detail",
+    });
+    const { data: commentData, run: commentRun } = useRequest(() =>
         getComment({ type: CommentType.PlayList, id: parseInt(id) }),
     );
     useEffect(() => {
         rankingDetailRun();
+        commentRun();
     }, [id]);
-
     return (
         <RankingWrapper className="wrap-v2">
             <RankingLeft />
@@ -38,15 +35,14 @@ const Ranking = memo(() => {
                         <Skeleton loading />
                     ) : (
                         <>
-                            <RankingInformation info={playList} />
+                            <RankingInformation info={res?.playlist} />
                             <RankingTable
-                                playCount={playList?.playCount}
-                                trackCount={playList?.trackCount}
-                                list={playList?.tracks}
+                                playCount={res?.playlist?.playCount}
+                                trackCount={res?.playlist?.trackCount}
+                                list={res?.playlist?.tracks}
                             />
                         </>
                     )}
-                    (
                     <CommentList
                         title="精彩评论"
                         isHot={false}
